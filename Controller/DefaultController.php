c77046ddb8c57970f3c1213fa5f545a02342a93d
Ken Marfilla <marfillaster@gmail.com>
2012-09-25 16:42:29 +0800

    forward cookie

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -9,27 +9,8 @@ class DefaultController extends Controller
 {
     public function indexAction($prefix, $app, $path)
     {
-        try {
-            $embedded = new Embedded($this->container);
-            $obLevel = $this->container->get('request')->headers->get('X-Php-Ob-Level') ?: 0;
-            $sfResponse = $embedded->serve($prefix, $app, $path);
+        $embed = new Embedded($this->container);
 
-            if ((ob_get_level() > $obLevel) && !$sfResponse->getContent()) {
-                $response = new \Symfony\Component\HttpFoundation\StreamedResponse(function () {
-                        echo ob_get_flush();
-                    },
-                    $response->getStatusCode(), $sfResponse->getHttpHeaders()
-                );
-            } else {
-                $response = new \Symfony\Component\HttpFoundation\Response(
-                    $sfResponse->getContent(),
-                    $sfResponse->getStatusCode(), $sfResponse->getHttpHeaders()
-                );
-            }
-
-            return $response;
-        } catch (\sfError404Exception $f) {
-            throw $this->createNotFoundException($f->getMessage());
-        }
+        return $embed->serve($prefix, $app, $path);
     }
 }
--- Embedded.php
+++ Embedded.php
@@ -4,7 +4,8 @@ namespace Butterweed\SF1EmbedderBundle;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Butterweed\SF1EmbedderBundle\Event\ContextEvent;
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Cookie;
 class Embedded
 {
     protected $container;
@@ -15,7 +16,7 @@ class Embedded
     }
 
     /**
-     * @return \sfWebResponse
+     * @return Response
      * @throws \sfError404Exception
      * @throws \sfException
      */
@@ -26,7 +27,7 @@ class Embedded
         $container = $this->container;
         $kernel = $container->get('kernel');
         $request = $container->get('request');
-
+        $obLevel = $container->get('request')->headers->get('X-Php-Ob-Level') ?: 0;
         $dispatcher = $container->get('event_dispatcher');
 
         define('SF2_EMBEDDED', true);
@@ -43,7 +44,13 @@ class Embedded
         $context = \sfContext::createInstance($configuration);
         $eventContext = new ContextEvent($context);
         $dispatcher->dispatch('butterweed_sf1_embedder.pre_dispatch', $eventContext);
-        $context->getController()->dispatch();
+
+        try {
+            $context->getController()->dispatch();
+        } catch (\sfError404Exception $f) {
+                throw new NotFoundHttpException($f->getMessage());
+        }
+
         $dispatcher->dispatch('butterweed_sf1_embedder.post_dispatch', $eventContext);
 
         // especially needed for response listeners such as debug bar
@@ -51,6 +58,34 @@ class Embedded
         $event = $context->getEventDispatcher()->filter(new \sfEvent($response, 'response.filter_content'), $response->getContent());
         $response->setContent($event->getReturnValue());
 
-        return $response;
+        if ((ob_get_level() > $obLevel) && !$response->getContent()) {
+            $resp = new \Symfony\Component\HttpFoundation\StreamedResponse(function () {
+                    echo ob_get_flush();
+                },
+                $response->getStatusCode(), $response->getHttpHeaders()
+            );
+        } else {
+            $resp = new \Symfony\Component\HttpFoundation\Response(
+                $response->getContent(),
+                $response->getStatusCode(), $response->getHttpHeaders()
+            );
+        }
+
+        foreach ($response->getCookies() as $cookie) {
+            $c = new Cookie(
+                $cookie['name'],
+                $cookie['value'],
+                in_numeric($cookie['expire']) ? (int) $cookie['expire'] : strtotime($cookie['expire']),
+                $cookie['path'],
+                $cookie['domain'],
+                $cookie['secure'],
+                $cookie['httpOnly']);
+
+            $resp->headers->setCookie($c);
+        }
+
+        return $resp;
     }
+
+
 }
